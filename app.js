e29df89471a2cab63c4e7c2f437804c58a23457a
Nghiemct69 <132883462+Nghiemct69@users.noreply.github.com>
2023-05-14 12:04:18 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// for (var i =10; i > 0; i--) {
+//      console.log (i);
+//      document.write(i + "<br>");
+// }
+
+
+// for (var i= 0; i < 10; i++){
+//      console.log((i+1)+". Hello world");
+//      document.write((i+1)+". Hello world" + "<br>");
+//     }
+
+
+//   for (var i =1; i <= 10; i++){
+//        console.log("Bảng cửu chương" + i);
+//        document.write("Bảng cửu chương" + i + "<br>");
+//   for (var r = 1; r <= 10; r++){
+//       console.log(i + "x" + r + "=" + i*r);
+//       document.write(i + "x" + r + "=" + i*r * "<br>");
+
+//   }
+
+//   } 
+
+
+// var i=0
+// while (i<10){
+//     console.log(i);
+//     document.write(i + "<br>");
+//     i++;
+// }
+
+
+var i =3
+while (i <3){
+    console.log( bt);
+    i++;
+
+}
+
+
+
+
+
